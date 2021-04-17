a131d86bd9a2201013b662a658f1af1ca9954b93
efmendes <ederferreiramendes@gmail.com>
2021-04-17 17:45:32 -0300

    Pacote genérico

---
+++ primeiro-programa-java/src/curso/java/glaucio/Pessoa.java
@@ -0,0 +1,17 @@
+package curso.java.glaucio;
+
+public class Pessoa {
+	
+	
+	private String nome;
+	
+	private String sobrenome;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void steNome(String nome) {
+		this.nome = nome;
+	}
+}
