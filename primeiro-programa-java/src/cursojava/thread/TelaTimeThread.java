b637ae10a4f77d2068cb333599bab39ebf007ac6
efmendes <ederferreiramendes@gmail.com>
2021-04-28 01:02:59 -0300

    Tela

--- primeiro-programa-java/src/cursojava/thread/TelaTimeThread.java
+++ primeiro-programa-java/src/cursojava/thread/TelaTimeThread.java
@@ -1,18 +1,50 @@
 package cursojava.thread;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class TelaTimeThread extends JDialog {
 	
-	public TelaTimeThread() {
+	private JPanel jPanel = new JPanel (new GridBagLayout());
+	
+	private JLabel descricaoHora = new JLabel ("Time Tread 1");
+	private JTextField mostratempo = new  JTextField();
+	
+	private JLabel descricaoHora2 = new JLabel ("Time Tread 2");
+	private JTextField mostratempo2 = new  JTextField();
+	
+	
+	public TelaTimeThread() { 
 		setTitle("Minha tela de time com thread");
-		setSize(new Dimension(240,240));
+		setSize(new Dimension(240, 240));
 		setLocationRelativeTo(null);
 		setResizable(false);
 		/*Primeira parte concluida*/
 		
+		
+		GridBagConstraints gridBagConstraints = new GridBagConstraints();
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 0;
+		
+		descricaoHora.setPreferredSize(new Dimension(200, 25));
+		jPanel.add(descricaoHora, gridBagConstraints);
+		
+		mostraTempo.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy ++;
+		jPanel.add(mostraTempo, gridBagConstraints);
+		
+		descricaoHora2.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy ++;
+		jPanel.add(descricaoHora2, gridBagConstraints);
+		
+		add(jPanel, BorderLayout.WEST);
 		/*último comando*/
 		setVisible(true);/*Tela visivel ao usuário*/
 	}
